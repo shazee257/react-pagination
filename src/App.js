0845c4b1fbd4af86d96f2fc361af58f017c5a47d
Muhammad Shahzad <shazee257@gmail.com>
2022-04-17 15:16:55 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -32,11 +32,7 @@ const App = () => {
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
       <Posts posts={currentPosts} loading={loading} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={paginate}
-      />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
     </div>
   );
 };
